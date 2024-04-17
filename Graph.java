a8366de2ab5ee4cd5f1f11a71c953608876884e1
Parker Vannah <160913409+Speed71P@users.noreply.github.com>
2024-04-17 10:13:18 -0700

    initialized variables used for the graph in the Graph class

---
+++ Graph.java
@@ -0,0 +1,13 @@
+public class Graph {
+    private int xMin;
+    private int xMax;
+    private int yMin;
+    private int yMax;
+
+    public Graph(int xMin, int xMax, int yMin, int yMax) {
+        this.xMin = xMin;
+        this.xMax = xMax;
+        this.yMin = yMin;
+        this.yMax = yMax;
+    }
+}
