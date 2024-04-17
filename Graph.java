7616f18add45d857c03650aaa507d180d110779d
Parker Vannah <160913409+Speed71P@users.noreply.github.com>
2024-04-17 19:02:09 +0000

    Added get functions for attributes, fixed bugs within the getYBand function, and added error messages for invalid x/y min/max values

--- Graph.java
+++ Graph.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 public class Graph {
     private int xMin;
     private int xMax;
@@ -9,6 +10,16 @@ public class Graph {
         this.xMax = xMax;
         this.yMin = yMin;
         this.yMax = yMax;
+
+        if (xMin > xMax) {
+            System.out.println("Error. The minimum x value cannot be larger than the maximum x value.");
+            System.exit(1);
+        }
+        
+        if (yMin > yMax) {
+            System.out.println("Error. The minimum y value cannot be larger than the maximum y value.");
+            System.exit(1);
+        }
     }
 
     // function to be graphed
@@ -16,13 +27,20 @@ public class Graph {
         return 5; // equation x = 5
     }
 
-    public boolean[] getYBand(int x) {
-        boolean[] boolArray = new boolean[];
-        for (int i = yMin; i < yMax; i++) {
+    public boolean[] getYBand(int y) {
+        boolean[] boolArray = new boolean[yMax - yMin];
+        for (int i = 0; i < boolArray.length; i++) {
             boolArray[i] = false;
         }
         if (f(x) >= yMin && f(x) <= yMax) {
-            boolArray[f(x)] = true;
+            boolArray[f(x) + Math.abs(yMin)] = true;
         }
+        return boolArray;
     }
+
+    // get attributes
+    public int getXMin() {return xMin;}
+    public int getXMax() {return xMax;}
+    public int getYMin() {return yMin;}
+    public int getYMax() {return yMax;}
 }
